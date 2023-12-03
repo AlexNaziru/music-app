bf280dfd898199058d782c8c622967a8eaa6fba2
Alex <alex.naziru.dev@gmail.com>
2023-12-03 20:10:49 +0200

    progress audio bar working

--- src/stores/player.js
+++ src/stores/player.js
@@ -1,14 +1,21 @@
 import { defineStore } from 'pinia'
 import { Howl, Howler } from 'howler'
+import helper from '../plugins/helper'
 
 // 'player' is an id
 export default defineStore('player', {
   state: () => ({
     current_song: {},
-    sound: null
+    sound: null,
+    seek: '00:00',
+    duration: '00:00',
+    playerProgress: '0%'
   }),
   actions: {
     async newSong(song) {
+      if (this.sound instanceof Howl) {
+        this.sound.unload()
+      }
       this.current_song = song
 
       if (this.sound) {
@@ -20,6 +27,9 @@ export default defineStore('player', {
         html5: true
       })
       this.sound.play()
+      this.sound.on('play', () => {
+        requestAnimationFrame(this.progress)
+      })
     },
 
     async toggleAudio() {
@@ -30,6 +40,28 @@ export default defineStore('player', {
           this.sound.play()
         }
       }
+    },
+    progress() {
+      this.seek = helper.formatTime(this.sound.seek())
+      this.duration = helper.formatTime(this.sound.duration())
+      this.playerProgress = `${(this.sound.seek() / this.sound.duration()) * 100}%`
+
+      if (this.sound.playing()) {
+        requestAnimationFrame(this.progress)
+      }
+    },
+    updateSeek(event) {
+      // this allows when you click on the progress music ball, to move it where you want
+      if (!this.sound.playing) {
+        return
+      }
+      const { x, width } = event.currentTarget.getBoundingClientRect()
+      const clickX = event.clientX - x
+      const percentage = clickX / width
+      const seconds = this.sound.duration() * percentage
+
+      this.sound.seek(seconds)
+      this.sound.once('seek', this.progress)
     }
   },
   getters: {
