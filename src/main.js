8a9b01b24fab797a9881c0fba2290a9ca135aa0c
unknown <alex.naziru.dev@gmail.com>
2023-12-04 21:47:14 +0200

    directives

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import App from './App.vue'
 import router from './router'
 import VeeValidatePlugin from './plugins/validation'
 import { auth } from './plugins/firebase'
+import Icon from './directives/icon'
 
 // this will automatically log in a user if he is registered and logged in.
 // if we didn't do this, each time we refresh the page, he will have to log again
@@ -21,6 +22,7 @@ auth.onAuthStateChanged(() => {
     app.use(createPinia())
     app.use(router)
     app.use(VeeValidatePlugin)
+    app.directive('icon', Icon)
 
     app.mount('#app')
   }
