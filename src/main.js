682c146e4fb204f507ab74251a44ee1c178563df
Alex <alex.naziru.dev@gmail.com>
2023-12-07 19:27:08 +0200

    cache

--- src/main.js
+++ src/main.js
@@ -10,9 +10,12 @@ import VeeValidatePlugin from './plugins/validation'
 import { auth } from './plugins/firebase'
 import Icon from './directives/icon'
 import i18n from './plugins/i18n'
+import { registerSW } from 'virtual:pwa-register'
 
 // this will automatically log in a user if he is registered and logged in.
 // if we didn't do this, each time we refresh the page, he will have to log again
+
+registerSW({ immediate: true })
 
 let app
 
