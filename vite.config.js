24fd323fb0bff6fcd6f95183eabb4eaf1565b5d6
Alex <alex.naziru.dev@gmail.com>
2023-12-13 20:46:08 +0200

    visualizer

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
+//import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -30,6 +31,7 @@ export default defineConfig({
         globPatterns: ['**/*.{js,css,html,png,jpg,}']
       }
     })
+    //visualizer({ open: true }) // This plugin will produce an HTML file for viewing the bundle size
   ],
   resolve: {
     alias: {
